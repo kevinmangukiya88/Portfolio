c78113ed7065ff45372ca1f2de872153a621c5be
snwork89 <snwork89@gmail.com>
2022-04-08 20:11:24 +0530

    initial commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
+
+const Projects = () => (
+  <Section id="projects">
+    <SectionDivider />
+    <SectionTitle main>Our Team ..</SectionTitle>
+    <GridContainer>
+      {projects.map((p, i) => {
+        return (
+          <BlogCard key={i} style={{padding:"10px 10px"}}>
+            <Img src={p.image} style={{ objectFit:"contain",height:"90%",width:"98%",borderRadius:"8px" }} />
+            <TitleContent>
+              <HeaderThree title>{p.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo className="card-info">{p.description}</CardInfo>
+            <div>
+              <TitleContent>App Development</TitleContent>
+              <TagList>
+                {p.tags.map((t, i) => {
+                  return <Tag key={i}>{t}</Tag>;
+                })}
+              </TagList>
+            </div>
+            {/*    <UtilityList>
+              <ExternalLinks href={p.visit}>Code</ExternalLinks>
+              <ExternalLinks href={p.source}>Source</ExternalLinks>
+            </UtilityList> */}
+          </BlogCard>
+        );
+      })}
+    </GridContainer>
+  </Section>
+);
+
+export default Projects;
---
+++ src/constants/constants.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    title: "Kevin Mangukiya",
+    image: "/images/kevin.jpeg",
+    tags: ["Flutter", "Firebase", "Photoshop"],
+
+    id: 0,
+  },
+  {
+    title: "Hardip Pokiya",
+    image: "/images/hardip.jpg",
+    tags: ["Flutter", "Firebase", "Photoshop"],
+    id: 1,
+  },
+  {
+    title: "Dhruv Bhagat",
+    image: "/images/dhruv.jpeg",
+    tags: ["Flutter", "Firebase", "Photoshop"],
+    id: 2,
+  },
+];
+
+export const TimeLineData = [
+  { year: "July-2020", text: "Started our journey" },
+  { year: 2020, text: "Shared our projects with the world" },
+  { year: "August-2021", text: "started our journy as a freelancers" },
+];
